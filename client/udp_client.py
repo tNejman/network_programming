a9ae9d053d362720ee6ce46753f9b07642c33fac
tnejman <01187260@pw.edu.pl>
2025-11-19 20:49:06 +0100

    Added initial files for the project; Dockerfiles require fixing

---
+++ client/udp_client.py
@@ -0,0 +1,112 @@
+import socket
+import sys
+import io
+import numpy as np
+from typing import Tuple
+import time
+import matplotlib.pyplot as plt
+import random as rand
+from math import log2
+
+HOST = '127.0.0.1'
+MAX_DATAGRAM_SIZE = 2 ** 1024
+POSSIBLE_SYMBOLS = ['R','E','G','G', 'I', 'N']
+
+def send_data(port: int, datagram_size: int) -> Tuple[bool, float]:
+	with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+		binary_stream = io.BytesIO()
+		for _ in range(datagram_size):
+			letter_to_be_written = POSSIBLE_SYMBOLS[rand.randint(0, len(POSSIBLE_SYMBOLS)-1)]
+			binary_stream.write(letter_to_be_written.encode('ascii'))
+		binary_stream.seek(0)
+		stream_data = binary_stream.read()
+		print("Sending buffer size= ", datagram_size)
+
+		start_time = time.perf_counter()
+		data = 0
+		try:
+			s.sendto(stream_data, (HOST, port))
+			data = s.recv(5)
+		except Exception as e:
+			print(e)
+			time.sleep(0.05)
+			return (False, -1)
+		end_time = time.perf_counter()
+		elapsed_time = end_time - start_time
+	
+		data_str = data.decode('ascii')
+  
+		if data_str == "OK":
+			print('Received', data_str)
+			s.close()
+			return (True, elapsed_time)
+		elif data_str == "ERROR":
+			print('Received', data_str)
+			s.close()
+			return (False, elapsed_time)
+		else:
+			raise Exception("Unknown server reponse")
+
+def find_max_datagram_size(port: int, initial_size: int = 2) -> Tuple[int, np.ndarray, np.ndarray]:
+	lower_bound = 0
+	test_size = initial_size
+	datagram_sizes: np.ndarray = np.array([])
+	times_measured: np.ndarray = np.array([])
+ 
+	while test_size <= MAX_DATAGRAM_SIZE:	
+		print(f"Testing doubling size: {test_size} bytes")	
+		print(f"Interation: {int(log2(test_size))}")
+  
+		is_response_ok, elapsed_time = send_data(port, test_size)
+		
+		if is_response_ok:
+			times_measured = np.append(times_measured, elapsed_time)
+			datagram_sizes = np.append(datagram_sizes, test_size)
+			lower_bound = test_size
+			test_size *= 2
+		else:
+			break
+
+	upper_bound = min(lower_bound*2, MAX_DATAGRAM_SIZE)
+ 
+	if upper_bound == lower_bound:
+		return (lower_bound, datagram_sizes, times_measured)
+
+	while lower_bound < upper_bound:
+		midpoint = (upper_bound + lower_bound) // 2
+	
+		is_response_ok, elapsed_time = send_data(port, midpoint)
+	
+		if is_response_ok:
+			times_measured = np.append(times_measured, elapsed_time)
+			datagram_sizes = np.append(datagram_sizes, test_size)
+			lower_bound = midpoint
+		else:
+			upper_bound = midpoint - 1
+
+	send_data(port, lower_bound)
+	return (lower_bound, datagram_sizes, times_measured)
+
+def main():
+	if len(sys.argv) < 2: 
+		print("no port, using 8888")
+		port=8888
+	else:
+		port = int( sys.argv[1] )
+
+	print("Will send to ", HOST, ":", port)
+
+	max_datagram_size, datagram_sizes, times_measured = find_max_datagram_size(port, 2)
+	print(f"Max datagram size is: {max_datagram_size}")
+
+	if (len(datagram_sizes) != len(times_measured)):
+		raise Exception(f"Datagram sizes is of size: {len(datagram_sizes)} but times measured is of size {len(times_measured)}")
+	plt.plot(datagram_sizes, times_measured)
+	plt.xlabel("datagram size")
+	plt.ylabel("time measured")
+	plt.savefig("datagram_size_vs_time_measured.png")
+
+	print('Client finished.')
+ 
+if __name__ == "__main__":
+    main()
---
+++ server/udp_server.c
@@ -0,0 +1,101 @@
+#define _POSIX_C_SOURCE 200112L
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define TRUE 1
+#define BSIZE 1024 * 1024
+
+#define bailout(s) {perror(s); exit(1); }
+#define notDone() TRUE
+
+#define DEF_PORT "8888"
+
+int moreWork(void) {
+    return 1;
+}
+
+int send_to_client(int sock,
+                   const struct sockaddr *client_addr,
+                   socklen_t client_len,
+                   const char *response)
+{
+    ssize_t sent = sendto(sock, response, strlen(response), 0,
+                          client_addr, client_len);
+    return (int)sent;
+}
+
+int main(int argc, char **argv) {
+    int sock, rval;
+    struct addrinfo *bindto_address;
+    struct addrinfo hints;
+    char buf[BSIZE];
+
+    struct sockaddr_storage client_addr;
+    socklen_t client_addr_len;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET; // IPv4 
+    hints.ai_socktype = SOCK_DGRAM; // UDP
+    hints.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo(NULL, DEF_PORT, &hints, &bindto_address) != 0)
+        bailout("getting local address");
+
+    sock = socket(bindto_address->ai_family,
+                  bindto_address->ai_socktype,
+                  bindto_address->ai_protocol);
+    if (sock == -1)
+        bailout("opening datagram socket");
+
+    if (bind(sock, bindto_address->ai_addr, bindto_address->ai_addrlen) == -1)
+        bailout("binding datagram socket");
+
+    freeaddrinfo(bindto_address);
+
+    printf("UDP server listening on port %s...\n", DEF_PORT);
+
+    do {
+        memset(buf, 0, BSIZE);
+        client_addr_len = sizeof(client_addr);
+
+        // receive
+        rval = recvfrom(sock, buf, BSIZE - 1, 0,
+                        (struct sockaddr *)&client_addr, &client_addr_len);
+
+        if (rval == -1) {
+            perror("recvfrom");
+            continue;
+        }
+
+        /* ensure null-terminated string */
+        if (rval >= 0 && rval < BSIZE)
+            buf[rval] = '\0';
+        else
+            buf[BSIZE - 1] = '\0';
+
+        printf("%s: -->%s\n", argv[0], buf);
+        printf("datagram size is: %ld", strlen(buf));
+
+        int success = send_to_client(sock,
+                                     (struct sockaddr *)&client_addr,
+                                     client_addr_len,
+                                     "OK");
+        printf("Read data successful. Sending: \"OK\"\n");
+        if (success == -1) {
+            printf("Send to client failed...\n");
+        }
+
+        fflush(stdout);
+
+    } while (moreWork());
+
+    close(sock);
+    return 0;
+}

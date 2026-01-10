befdc604bf3adeccd3b796b74d73efb23856ee01
MDKIP <34871439+MDKIP@users.noreply.github.com>
2026-01-10 19:29:40 +0100

    Communication for all message types works

--- projekt/client/tcp_client.py
+++ projekt/client/tcp_client.py
@@ -1,6 +1,82 @@
 import socket
 import struct
 import random
+import hashlib
+import hmac
+
+MESSAGE_HMAC_SIZE = 28
+MESSAGE_TYPE_SIZE = 4
+MESSAGE_SIZE_SIZE = 8
+MESSAGE_HEADER_SIZE = MESSAGE_HMAC_SIZE + MESSAGE_TYPE_SIZE + MESSAGE_SIZE_SIZE
+
+TYPE_STANDARD_ENCRYPTED = 1
+TYPE_END_SESSION = 3
+TYPE_SERVER_OK = 2
+CONTENT_END_SESSION = "ENDSSION"
+
+def recv_exactly(conn, n):
+    data = b''
+    while len(data) < n:
+        packet = conn.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+def send_encrypted_message(sock, K, session_prng, message_type, message_text):
+    keystream_header = session_prng.randbytes(MESSAGE_HEADER_SIZE)
+    encoded_message = message_text.encode()
+    msg_size = len(encoded_message)
+    keystream_content = session_prng.randbytes(msg_size)
+
+    
+    msg_type = message_type
+    
+    encrypted_message = bytes(a ^ b for a, b in zip(encoded_message, keystream_content))
+    k_bytes = K.to_bytes((K.bit_length() + 7) // 8 or 1, byteorder='big')
+            
+    calculated_hmac = hmac.new(k_bytes, encrypted_message, hashlib.sha224).digest()
+    msg_header = struct.pack('!28sIQ', calculated_hmac, msg_type, msg_size)
+    encrypted_header = bytes(a ^ b for a, b in zip(msg_header, keystream_header))
+    msg = encrypted_header + encrypted_message
+    print(f'[Klient] Zakodowana wiadomość text=\"{message_text}\" hmac={calculated_hmac} ')
+    sock.sendall(msg)
+
+def check_for_server_ok_message(conn, K, session_prng, expected_length):
+    encrypted_header = recv_exactly(conn, MESSAGE_HEADER_SIZE)
+    if not encrypted_header:
+        return False
+
+    keystream_header = session_prng.randbytes(MESSAGE_HEADER_SIZE)
+    decrypted_header = bytes(a ^ b for a, b in zip(encrypted_header, keystream_header))
+
+    recv_hmac, msg_type, msg_size = struct.unpack('!28sIQ', decrypted_header)
+
+    if msg_size > 0:
+        encrypted_content = recv_exactly(conn, msg_size)
+        if not encrypted_content:
+            return False
+        
+        keystream_content = session_prng.randbytes(msg_size)
+        decrypted_content = bytes(a ^ b for a, b in zip(encrypted_content, keystream_content))
+    else:
+        decrypted_content = b''
+    
+    payload_to_verify = encrypted_content
+    k_bytes = K.to_bytes((K.bit_length() + 7) // 8 or 1, byteorder='big')
+    
+    calculated_hmac = hmac.new(k_bytes, payload_to_verify, hashlib.sha224).digest()
+
+    if hmac.compare_digest(calculated_hmac, recv_hmac):
+        # Autoryzacja OK
+        recv_message_length = struct.unpack('!Q', decrypted_content)[0]
+        if msg_type == TYPE_SERVER_OK and expected_length == recv_message_length:
+            print(f"[Klient] Otrzymano potwierdzenie otrzymania wiadomości")
+            return True
+        else:
+            return False
+    else:
+        return False
 
 def simple_tcp_client():
     host = '127.0.0.1'
@@ -37,7 +113,17 @@ def simple_tcp_client():
         K = pow(B, a_priv, p)
         print(f"[Klient] Wyliczono wspólny klucz: K='{K}'")
         
-        seesion_prng = random.Random(K)
+        session_prng = random.Random(K)
+
+        message_text = input('Podaj wiadomość:')
+        message_text_length = len(message_text)
+        
+        send_encrypted_message(sock, K, session_prng, TYPE_STANDARD_ENCRYPTED, message_text)
+
+        while not check_for_server_ok_message(sock, K, session_prng, message_text_length):
+            continue
+
+        send_encrypted_message(sock, K, session_prng, TYPE_END_SESSION, CONTENT_END_SESSION)
         
         
 
--- projekt/server/tcp_server.py
+++ projekt/server/tcp_server.py
@@ -16,6 +16,78 @@ MESSAGE_TYPE_SIZE = 4
 MESSAGE_SIZE_SIZE = 8
 MESSAGE_HEADER_SIZE = MESSAGE_HMAC_SIZE + MESSAGE_TYPE_SIZE + MESSAGE_SIZE_SIZE
 
+TYPE_STANDARD_ENCRYPTED = 1
+TYPE_END_SESSION = 3
+TYPE_SERVER_OK = 2
+CONTENT_END_SESSION = "ENDSSION"
+CONTENT_SERVER_OK = "SRVOK"
+
+def send_encrypted_message(sock, K, session_prng, message_type, message_bytes):
+    keystream_header = session_prng.randbytes(MESSAGE_HEADER_SIZE)
+    encoded_message = message_bytes
+    msg_size = len(encoded_message)
+    keystream_content = session_prng.randbytes(msg_size)
+
+    
+    msg_type = message_type
+    
+    encrypted_message = bytes(a ^ b for a, b in zip(encoded_message, keystream_content))
+    k_bytes = K.to_bytes((K.bit_length() + 7) // 8 or 1, byteorder='big')
+            
+    calculated_hmac = hmac.new(k_bytes, encrypted_message, hashlib.sha224).digest()
+    msg_header = struct.pack('!28sIQ', calculated_hmac, msg_type, msg_size)
+    encrypted_header = bytes(a ^ b for a, b in zip(msg_header, keystream_header))
+    msg = encrypted_header + encrypted_message
+    print(f'[Klient] Zakodowana wiadomość payload=\"{encoded_message}\" hmac={calculated_hmac} ')
+    sock.sendall(msg)
+
+def recive_encrypted_message(conn, session_prng, K, addr):
+    encrypted_header = recv_exactly(conn, MESSAGE_HEADER_SIZE)
+    if not encrypted_header:
+        return True
+
+    keystream_header = session_prng.randbytes(MESSAGE_HEADER_SIZE)
+    decrypted_header = bytes(a ^ b for a, b in zip(encrypted_header, keystream_header))
+
+    recv_hmac, msg_type, msg_size = struct.unpack('!28sIQ', decrypted_header)
+
+    if msg_size > 0:
+        encrypted_content = recv_exactly(conn, msg_size)
+        if not encrypted_content:
+            return False
+        
+        keystream_content = session_prng.randbytes(msg_size)
+        decrypted_content = bytes(a ^ b for a, b in zip(encrypted_content, keystream_content))
+    else:
+        decrypted_content = b''
+    
+    payload_to_verify = encrypted_content
+    k_bytes = K.to_bytes((K.bit_length() + 7) // 8 or 1, byteorder='big')
+    
+    calculated_hmac = hmac.new(k_bytes, payload_to_verify, hashlib.sha224).digest()
+
+    print(f'[{addr}] Odebrano dekodowaną wiadomość calculated_hmac=\"{calculated_hmac}\" recv_hmac={recv_hmac} ')
+
+    if hmac.compare_digest(calculated_hmac, recv_hmac):
+        # Autoryzacja OK
+        if msg_type == 3: # EndSession [cite: 60]
+            print(f"[{addr}] Otrzymano EndSession. Zamykanie.")
+            return False
+        else:
+            try:
+                print(f"[{addr}] Wiadomość: {decrypted_content.decode('utf-8').rstrip(chr(0))}") # usuwamy null bytes
+
+                send_encrypted_message(conn, K, session_prng, TYPE_SERVER_OK, msg_size.to_bytes(8, byteorder="big"))
+                return True
+            except BaseException as e:
+                print(f"[{addr}] Exception occurred while decoding: {type(e).__name__}: {e}")
+                print(f"[{addr}] Wiadomość binarna o rozmiarze {msg_size}")
+                return True
+    else:
+        print(f"[{addr}] BŁĄD INTEGRALNOŚCI! Odrzucono wiadomość.")
+        # W prawdziwym systemie tutaj zrywamy połączenie
+        return False
+
 def recv_exactly(conn, n):
     data = b''
     while len(data) < n:
@@ -53,46 +125,12 @@ def handle_client(conn, addr):
         response = struct.pack('!4sQ', SEVER_HELLO_SIGNATURE.encode(), B)
         conn.sendall(response)
 
-        while True:            
-            encrypted_header = recv_exactly(conn, MESSAGE_HEADER_SIZE)
-            if not encrypted_header:
-                break
+        continue_communication = True
+        while continue_communication:            
+            continue_communication = recive_encrypted_message(conn, session_prng, K, addr)
+            if not continue_communication:
+                print("communication will be not continued")
 
-            keystream_header = session_prng.randbytes(MESSAGE_HEADER_SIZE)
-            decrypted_header = bytes(a ^ b for a, b in zip(encrypted_header, keystream_header))
-
-            recv_hmac, msg_type, msg_size = struct.unpack('!28sIQ', decrypted_header)
-
-            if msg_size > 0:
-                encrypted_content = recv_exactly(conn, msg_size)
-                if not encrypted_content:
-                    break
-                
-                keystream_content = session_prng.randbytes(msg_size)
-                decrypted_content = bytes(a ^ b for a, b in zip(encrypted_content, keystream_content))
-            else:
-                decrypted_content = b''
-            
-            payload_to_verify = struct.pack('!IQ', msg_type, msg_size) + decrypted_content
-
-            k_bytes = K.to_bytes((K.bit_length() + 7) // 8 or 1, byteorder='big')
-            
-            calculated_hmac = hmac.new(k_bytes, payload_to_verify, hashlib.sha224).digest()
-
-            if hmac.compare_digest(calculated_hmac, recv_hmac):
-                # Autoryzacja OK
-                if msg_type == 3: # EndSession [cite: 60]
-                    print(f"[{addr}] Otrzymano EndSession. Zamykanie.")
-                    break
-                else:
-                    try:
-                        print(f"[{addr}] Wiadomość: {decrypted_content.decode('utf-8').rstrip(chr(0))}") # usuwamy null bytes
-                    except:
-                        print(f"[{addr}] Wiadomość binarna o rozmiarze {msg_size}")
-            else:
-                print(f"[{addr}] BŁĄD INTEGRALNOŚCI! Odrzucono wiadomość.")
-                # W prawdziwym systemie tutaj zrywamy połączenie
-                break
 
     except Exception as e:
         print(f"[{addr}] Błąd obsługi: {e}")
